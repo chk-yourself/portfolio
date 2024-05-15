857ec64151c3fc8fb098f299f3d57686d8d1b047
chk-yourself <c.hyunakim@gmail.com>
2024-05-15 12:15:29 -0700

    update resume

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -6,18 +6,21 @@ const About = () => {
     <HomeSection title="About" id="about">
       <div className="mx-auto my-0 md:mr-0 md:max-w-md">
         <p>
-          Frontend engineer with over 7 years of experience in crafting
-          impactful, consumer-facing digital experiences, specializing in
-          marrying technical functionality with intuitive, user-centered design.
+          Self-taught, user-centric web engineer with 7+ years of experience in
+          crafting impactful, consumer-facing digital experiences.
         </p>
+        {/*
         <p className="mt-4">
-          Self-taught, lifelong learner and tenacious problem solver with a
-          trained eye for detail, stemming from a robust foundation in UX
+          Starting my journey in UX/UI design, I specialize in marrying
+          technical functionality with intuitive, user-centered
+          design. As a self-taught, lifelong learner and tenacious problem solver with
+          a trained eye for detail, stemming from a robust foundation in UX
           design.
         </p>
         <p className="mt-4">
-          Adept in leveraging cutting-edge technologies and methodologies to
-          meet evolving user demands in line with the latest web standards.
+          Adept in leveraging modern technologies and fostering team synergy to
+          drive continuous innovation and refinement of the user experience, in
+          tune with evolving user needs, business objectives, and web standards.
         </p>
         <p className="mt-4">
           Proven leadership in guiding cross-functional teams to innovate and
@@ -25,6 +28,7 @@ const About = () => {
           objectives, committed to fostering a culture of continuous improvement
           and innovation.
         </p>
+        */}
       </div>
     </HomeSection>
   );
